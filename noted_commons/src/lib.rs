2b4dc191133e74b6e4a70ce42177792f0a07dcf3
Christian Bergschneider <magiclake@gmx.de>
2024-02-01 23:55:01 +0100

    feat: use wgpu for displaying a solid color for now
    At first, I wanted to use platform-native libraries but those don't work well together. That would mean I'd have to build a big compatibility layer, which I didn't want.

--- noted_commons/src/lib.rs
+++ noted_commons/src/lib.rs
@@ -1,49 +1,200 @@
-use raw_window_handle::WindowHandle;
-use rendering_engine::Size;
+use std::time::Instant;
 
-pub mod rendering_engine;
+use winit::{
+    dpi::PhysicalSize,
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::{Theme, Window, WindowBuilder},
+};
 
-pub struct MyRenderer<'a> {
-    surface: wgpu::Surface<'a>,
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub struct NotedRenderingEngine {
+    surface: wgpu::Surface,
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
-    size: Size,
-    handle: WindowHandle<'a>,
+    size: PhysicalSize<u32>,
+    window: Window,
 }
 
-impl <'a> MyRenderer<'a> {
-    fn new(handle: WindowHandle<'a>) -> Self {
-        
+impl NotedRenderingEngine {
+    async fn new(window: Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        });
+
+        let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+        let all_adapters = instance.enumerate_adapters(wgpu::Backends::all());
+
+        let adapter = all_adapters
+            .filter(|adapter| adapter.is_surface_supported(&surface))
+            .next()
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    label: None,
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .copied()
+            .filter(|it| it.is_srgb())
+            .next()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        surface.configure(&device, &config);
+
+        NotedRenderingEngine {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            window: window,
+        }
     }
-}
 
-impl rendering_engine::SoftwareRenderer for MyRenderer<'_> {
-    fn redraw(&self) -> Result<(), Box<dyn std::error::Error>> {
+    fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn input(&self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&self) {
+        // Update logic here
+    }
+
+    fn redraw(&self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
         Ok(())
     }
 }
 
-
-pub struct NotedApplication<Renderer: rendering_engine::SoftwareRenderer, Engine: rendering_engine::RenderingEngine<Renderer>> {
-    pub engine: Engine,
-    pub renderer: Renderer,
-}
-
-impl <Renderer: rendering_engine::SoftwareRenderer, Engine: rendering_engine::RenderingEngine<Renderer>> NotedApplication<Renderer, Engine> {
-    pub fn new(engine: Engine, renderer: Renderer) -> Self {
-        Self {
-            engine,
-            renderer,
-        }
-    }
-
-    pub fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
-        self.engine.start_loop(&self.renderer)
-    }
-}
-
-
-pub fn init() {
+pub async fn run_noted() {
     env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title(format!("Noted! {}", VERSION))
+        .with_min_inner_size(winit::dpi::LogicalSize::new(640.0, 480.0))
+        .with_theme(Some(Theme::Dark))
+        .build(&event_loop)
+        .unwrap();
+
+    let mut engine = NotedRenderingEngine::new(window).await;
+
+    event_loop.run(move |base_event, _, control_flow| {
+        *control_flow = winit::event_loop::ControlFlow::WaitUntil(
+            Instant::now() + std::time::Duration::from_millis(32), // 30 FPS if we're not doing anything
+        );
+
+        match base_event {
+            Event::WindowEvent {
+                event, window_id, ..
+            } if engine.window().id() == window_id => {
+                if !engine.input(&event) {
+                    // Prioritize input
+                    match event {
+                        WindowEvent::CloseRequested => {
+                            *control_flow = winit::event_loop::ControlFlow::Exit;
+                        }
+                        WindowEvent::Resized(new_size) => {
+                            engine.resize(new_size);
+                        }
+                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                            engine.resize(*new_inner_size);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            Event::RedrawRequested(window_id) if engine.window().id() == window_id => {
+                engine.update();
+                match engine.redraw() {
+                    Ok(_) => (),
+                    Err(wgpu::SurfaceError::Lost) => {
+                        engine.resize(engine.size);
+                    }
+                    Err(wgpu::SurfaceError::OutOfMemory) => {
+                        *control_flow = winit::event_loop::ControlFlow::Exit;
+                    }
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            },
+            Event::MainEventsCleared => {
+                engine.window().request_redraw();
+            }
+            _ => (),
+        }
+    });
 }
--- noted_linux/src/main.rs
+++ noted_linux/src/main.rs
@@ -1,11 +1,3 @@
-use noted_commons::MyRenderer;
-
-mod rendering;
-
 fn main() {
-    let engine = rendering::LinuxRenderingEngineImpl::new();
-    let renderer = MyRenderer;
-
-    let app = noted_commons::NotedApplication::new(engine, renderer);
-    app.run().unwrap();
+    pollster::block_on(noted_commons::run_noted())
 }
